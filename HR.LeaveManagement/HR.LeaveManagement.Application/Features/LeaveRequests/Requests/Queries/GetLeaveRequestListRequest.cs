851d0d9a9ade4856a71c755510e41369d1d5e16e
EgorAntonovich <egorantonovich@coherentsolutions.com>
2022-12-26 01:04:15 +0300

    Add requests and handlers

---
+++ HR.LeaveManagement/HR.LeaveManagement.Application/Features/LeaveRequests/Requests/Queries/GetLeaveRequestListRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using HR.LeaveManagement.Application.DTOs.LeaveRequest;
+using HR.LeaveManagement.Domain;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequests.Requests.Queries
+{
+    public class GetLeaveRequestListRequest : IRequest<List<LeaveRequestListDto>>
+    {
+        
+    }
+}
