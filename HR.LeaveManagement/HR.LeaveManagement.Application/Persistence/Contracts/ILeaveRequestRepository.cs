c40ccabf4905f8dd3286d8a78eb5632a31ee2d13
EgorAntonovich <egorantonovich@coherentsolutions.com>
2022-12-26 01:05:04 +0300

    Add MediatR, methods for interfaces

--- HR.LeaveManagement/HR.LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
+++ HR.LeaveManagement/HR.LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -1,9 +1,12 @@
-﻿using HR.LeaveManagement.Domain;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HR.LeaveManagement.Domain;
 
 namespace HR.LeaveManagement.Application.Persistence.Contracts
 {
     public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
     {
-        
+        Task<LeaveRequest> GetLeaveRequestWithDetails(int Id);
+        Task<List<LeaveRequest>> GetLeaveRequestsWithDetails();
     }
 }
