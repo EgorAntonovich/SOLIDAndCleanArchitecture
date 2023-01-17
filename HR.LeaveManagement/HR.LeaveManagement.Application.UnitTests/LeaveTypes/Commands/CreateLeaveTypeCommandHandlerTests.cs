84d0b98ba8af177c3e489eab8e817ed960db234e
EgorAntonovich <egorantonovich@coherentsolutions.com>
2023-01-17 23:58:35 +0100

    Add unit tests

---
+++ HR.LeaveManagement/HR.LeaveManagement.Application.UnitTests/LeaveTypes/Commands/CreateLeaveTypeCommandHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Contracts.Persistence.DTOs.LeaveType;
+using HR.LeaveManagement.Application.Contracts.Persistence.Features.LeaveTypes.Handlers.Commands;
+using HR.LeaveManagement.Application.Contracts.Persistence.Features.LeaveTypes.Requests.Commands;
+using HR.LeaveManagement.Application.Contracts.Persistence.Profiles;
+using HR.LeaveManagement.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace HR.LeaveManagement.Application.UnitTests.LeaveTypes.Commands;
+
+public class CreateLeaveTypeCommandHandlerTests
+{
+    private readonly IMapper _mapper;
+    private readonly Mock<ILeaveTypeRepository> _mockRepo;
+    private readonly CreateLeaveTypeDto _leaveTypeDto;
+
+    public CreateLeaveTypeCommandHandlerTests()
+    {
+        _mockRepo = MockLeaveTypeRepositories.GetLeaveTypeRepository();
+        var mapperConfig = new MapperConfiguration(c =>
+        {
+            c.AddProfile<MappingProfile>();
+        });
+
+        _mapper = mapperConfig.CreateMapper();
+        _leaveTypeDto = new CreateLeaveTypeDto
+        {
+            DefaultDays = 15,
+            Name = "Test DTO"
+        };
+    }
+
+    [Fact]
+    public async Task Valid_LeaveType_Added()
+    {
+        var handler = new CreateLeaveTypeCommandHandler(_mockRepo.Object, _mapper);
+
+        var result = await handler.Handle(new CreateLeaveTypeCommand() {LeaveType = _leaveTypeDto}, CancellationToken.None);
+
+        var leaveTypes = await _mockRepo.Object.GetAll();
+        
+        result.ShouldBeOfType<int>();
+        leaveTypes.Count.ShouldBe(4);
+    }
+}
---
+++ HR.LeaveManagement/HR.LeaveManagement.Application.UnitTests/Mocks/MockLeaveTypeRepositories.cs
@@ -0,0 +1,45 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain;
+using Moq;
+
+namespace HR.LeaveManagement.Application.UnitTests.Mocks;
+
+public class MockLeaveTypeRepositories
+{
+    public static Mock<ILeaveTypeRepository> GetLeaveTypeRepository()
+    {
+        var leaveTypes = new List<LeaveType>
+        {
+            new LeaveType
+            {
+                Id = 1,
+                DefaultDays = 10,
+                Name = "Test Vacation"
+            },
+            new LeaveType
+            {
+                Id = 2,
+                DefaultDays = 15,
+                Name = "Test Sick"
+            },
+            new LeaveType
+            {
+                Id = 3,
+                DefaultDays = 15,
+                Name = "Test Maternity"
+            }
+        };
+
+        var moqRepo = new Mock<ILeaveTypeRepository>();
+
+        moqRepo.Setup(r => r.GetAll()).ReturnsAsync(leaveTypes);
+
+        moqRepo.Setup(r => r.Add(It.IsAny<LeaveType>())).ReturnsAsync((LeaveType leaveType) =>
+        {
+            leaveTypes.Add(leaveType);
+            return leaveType;
+        });
+
+        return moqRepo;
+    }
+}
