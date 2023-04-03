392c44631740bf423c63956c6a6454de846c637a
EgorAntonovich <egorantonovich@coherentsolutions.com>
2023-04-03 23:27:58 +0300

    Add email service.

--- HR.LeaveManagementSystem/HR.LeaveManagementSystem.Infrastructure/InfrastructureServicesRegistration.cs
+++ HR.LeaveManagementSystem/HR.LeaveManagementSystem.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using HR.LeaveManagementSystem.Application.Contracts.Email;
+using HR.LeaveManagementSystem.Application.Models.Email;
+using HR.LeaveManagementSystem.Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HR.LeaveManagementSystem.Infrastructure;
 
 public static class InfrastructureServicesRegistration
 {
-    public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services,
-        IConfiguration configuration)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailSender, EmailSender>();
         return services;
     }
 }
